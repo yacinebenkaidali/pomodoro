14d4ef7956a30fa75a8d580a791d817be4214409
yacinebenkaidali <fy_benkaidali@esi.dz>
2024-03-10 11:02:00 +0100

    Implemented round mechanism

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/yacinebenkaidali/pomodoro
 go 1.21.3
 
 require (
+	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/schollz/progressbar/v3 v3.14.2 // indirect
--- main.go
+++ main.go
@@ -2,23 +2,114 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
 )
 
+const workMinutes = 5
+const restMinutes = 5
+const nbRounds = 2
+
 func main() {
+	workBar := progressbar.NewOptions(workMinutes,
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(false),
+		progressbar.OptionSetWidth(25),
+		progressbar.OptionSetDescription("[red][Work session progress][reset]"),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[red]=[reset]",
+			SaucerHead:    "[red]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+	)
+	restBar := progressbar.NewOptions(restMinutes,
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(false),
+		progressbar.OptionSetWidth(25),
+		progressbar.OptionSetDescription("[blue][Take a break, you deserve it][reset]"),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[blue]=[reset]",
+			SaucerHead:    "[blue]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+	)
+
 	done := make(chan struct{})
-	count := 0
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
-		for v := range time.Tick(1 * time.Second) {
-			fmt.Printf("%v\n", v.Format("4:05"))
-			count++
-			if count == 10 {
-				done <- struct{}{}
+		for i := 0; i < nbRounds; i++ {
+			now := time.Now()
+			workBar.Reset()
+			workBar.RenderBlank()
+		workFree:
+			for {
+				select {
+				case <-time.Tick(1 * time.Second):
+					{
+						count := minutesSince(now)
+						workBar.Add(1)
+						if count > workMinutes {
+							break workFree
+						}
+					}
+				case <-sigs:
+					{
+						done <- struct{}{}
+						return
+					}
+				}
 			}
+			now = time.Now()
+			restBar.Reset()
+			restBar.RenderBlank()
+		restFree:
+			for {
+				select {
+				case <-time.Tick(1 * time.Second):
+					{
+						count := minutesSince(now)
+						restBar.Add(1)
+						if count > restMinutes {
+							break restFree
+						}
+					}
+				case <-sigs:
+					{
+						done <- struct{}{}
+						return
+					}
+				}
+			}
+			//make a sound here
+			fmt.Print("\033[H\033[2J") //clear the console
 		}
+		done <- struct{}{}
 	}()
+
+	// go func() {
+	// 	<-sigs
+	// 	done <- struct{}{}
+	// }()
 	<-done
 
-	fmt.Println("10 seconds have passed")
+	fmt.Printf("\n %d round have passed\n", nbRounds)
+}
+
+func minutesSince(t time.Time) int {
+	minutes := time.Since(t).Seconds()
+	// minutes := time.Since(t).Minutes()
+	return int(minutes)
 }
